388164b0cf3c408836a30e883a659ef053370c8b
mwmajewsk <5279578+mmajewsk@users.noreply.github.com>
2022-12-06 06:14:44 +0100

    delete some things

--- 6/t6.py
+++ 6/t6.py
@@ -8,24 +8,6 @@ from day import Day
 import os
 
 
-from itertools import zip_longest
-# from itertools import groupby
-
-def grouper(iterable, n, *, incomplete='fill', fillvalue=None):
-    "Collect data into non-overlapping fixed-length chunks or blocks"
-    # grouper('ABCDEFG', 3, fillvalue='x') --> ABC DEF Gxx
-    # grouper('ABCDEFG', 3, incomplete='strict') --> ABC DEF ValueError
-    # grouper('ABCDEFG', 3, incomplete='ignore') --> ABC DEF
-    args = [iter(iterable)] * n
-    if incomplete == 'fill':
-        return zip_longest(*args, fillvalue=fillvalue)
-    if incomplete == 'strict':
-        return zip(*args, strict=True)
-    if incomplete == 'ignore':
-        return zip(*args)
-    else:
-        raise ValueError('Expected fill, strict, or ignore')
-
 def p1(cnt):
     day = Day(6)
     # day = Day(6,'pb.in')
